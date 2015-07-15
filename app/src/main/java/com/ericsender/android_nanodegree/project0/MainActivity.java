6694045f7ebe3d8a09fac2e9fbe57b2705083c36
Eric K. Sender <sender.eric@gmail.com>
2015-07-15 11:55:02 -0700

    Implemented styles to factor out comment elements in layout.

--- app/src/main/java/com/ericsender/android_nanodegree/project0/MainActivity.java
+++ app/src/main/java/com/ericsender/android_nanodegree/project0/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ericsender.android_nanodegree.project0;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,7 +11,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +19,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void sendMessage(View view) {
         Context context = getApplicationContext();
